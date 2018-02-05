1d2eb8e084cc00046d92d00bc53f4ea51c563a18
缝衣针 <huo_shao_feng@163.com>
2018-02-05 17:03:05 +0800

    Update SearchService.java

--- huohuo-search/src/main/java/com/huohuo/search/service/SearchService.java
+++ huohuo-search/src/main/java/com/huohuo/search/service/SearchService.java
@@ -3,5 +3,7 @@ package com.huohuo.search.service;
 import com.huohuo.search.pojo.SearchResult;
 
 public interface SearchService {
+	//update on github 
 	SearchResult search(String queryString, int page, int rows) throws Exception ;
+	//let see whether this change will show on eclipse or not
 }
